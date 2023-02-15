458075a2f11c62dac58f2dc8d488d26105c2331c
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2023-02-15 15:05:13 +0100

    Update graphe.py

--- graphe.py
+++ graphe.py
@@ -1,4 +1,5 @@
 from mdp import *
+import numpy as np
 
 class graphe(gramPrintListener) :
 
@@ -9,8 +10,13 @@ class graphe(gramPrintListener) :
         parser = gramParser(stream)
         tree = parser.program()
         walker = ParseTreeWalker()
-        walker.walk(self, tree) 
-        pass
+        walker.walk(self, tree)
+        self.dictStates = {}
+        for i, state in enumerate(self.states):
+            self.dictStates[state] = i
+        self.dictActions = {}
+        for i, action in enumerate(self.actions):
+            self.dictActions[action] = i
 
     def __repr__(self) : 
         ret = ""
@@ -22,7 +28,46 @@ class graphe(gramPrintListener) :
             ret +="Transition from " + x[0] + " with no action and targets " + str(x[1]) + " with weights " + str(x[2]) + "\n"
         return ret
 
-    def grapheToMat(self) : 
+    def _grapheTransNotActToMatAdj(self) -> np.ndarray: 
+        matAdjNoAct = np.zeros((len(self.states), len(self.states)))
+        for transnoact in self.transnoact:
+            for arrivee, poids in zip(transnoact[1], transnoact[2]):
+                ligne = self.dictStates[transnoact[0]]
+                colonne = self.dictStates[arrivee]
+                matAdjNoAct[ligne, colonne] = poids
+        return matAdjNoAct
+    
+    def _grapheTransActToMatAdj(self) -> np.ndarray:
+        matAdjAct = np.zeros((len(self.actions), len(self.states), len(self.states)))
+        for transact in self.transact:
+            for arrivee, poids in zip(transact[2], transact[3]):
+                coor_x = self.dictActions[transact[1]]
+                coor_y = self.dictStates[transact[0]]
+                coor_z = self.dictStates[arrivee]
+                matAdjAct[coor_x, coor_y, coor_z] = poids
+        return matAdjAct
+    
+    def grapheToMat(self) -> np.ndarray:
+        """
+        Fonction appelée qui permet de définir la matrice du graphe.
+        Elle prend en compte la présence ou non de transition avec ou sans actions.
+        return:
+            np.ndarray de taille nb_actions, nb_etats, nb_etats
+        """
+        pattern = (self.transact, self.transnoact)
+        match pattern:
+            case ([], []):
+                return np.zeros(0)
+            case (_, []):
+                return self._grapheTransActToMatAdj()
+            case ([], _):
+                return np.expand_dims(self._grapheTransNotActToMatAdj(), 1)
+            case _:
+                mat = self._grapheTransActToMatAdj()
+                for i in range(mat.shape[0]):
+                    mat[i] += self._grapheTransNotActToMatAdj()
+                return mat
+
+
+    def grapheToMatInc(self) : 
         pass
-
-
