5194a6ecd40621e7c2738a59bcf1d128dbac75cb
Vincent Sevestre <viviseve9@gmail.com>
2023-03-15 15:34:11 +0100

    Découpage de nos fonctions

--- graphe.py
+++ graphe.py
@@ -5,9 +5,7 @@ import graphviz
 import moviepy.editor as mpy
 import os
 import shutil
-import pandas as pd
-# TODO: implémenter calcul probas pour mdp et mc (avec s0, s1 et s?), utiliser scipy.linprog pour mdp
-# TODO: implémenter calcul récompenses pour mdp et mc
+
 # TODO: créer des boucles aux dead ends
 
 
@@ -390,100 +388,3 @@ class graphe(gramPrintListener):
         if make_gif:
             self._create_gif()
         return chemin
-    
-    def _create_Matrix(self):
-        pass # TODO
-
-    def _create_vector(self):
-        pass # TODO
-
-    def pctl_finally(self, state):
-        assert self.transact == [], "Il ne s'agit pas d'une MC"
-        A = self._create_Matrix()
-        b = self._create_vector()
-        result = np.linalg.inv(np.eye(len(A)) - A)@b
-        return result
-
-    def pctl_finally_max_bound(self, state, max_bound: int):
-        assert self.transact == [], "Il ne s'agit pas d'une MC"
-        A = self._create_Matrix()
-        b = self._create_vector()
-        x0 = np.zeros((len(A), 1))
-        for _ in range(max_bound):
-            x0 = A@x0 + b
-        return x0
-
-    def pctl_mdp(self):
-        assert self.transnoact == [], "Il ne s'agit pas d'une MDP"
-        # TODO
-
-    def statistiques(self, N_pas=50, N_parcours=50):
-        """
-        Étude statistique de graphe par parcours aléatoires
-        N_pas : Nombre de pas à effectuer dans le graphe
-        N_parcours : Nombre de parcours 
-        """
-
-        header1 = self.states + ["Total"]
-        header2 = ["->" + state for state in self.states] + ["Total"]
-        ll = len(header1) + 1
-        freq = [[0 for _ in range(ll)] for _ in range(ll)]
-        freq = np.array(freq)
-
-        for _ in range(N_parcours):
-            chemin = self.parcours(regle="alea", N_pas=N_pas, ret_chemin=True)
-            old_state = 0
-            for state in chemin[1:]:
-                freq[old_state + 1, state + 1] += 1
-                old_state = state
-
-        freq = freq / (N_pas * N_parcours)
-        freq[-1] = np.sum(freq, axis=0)
-        freq[:, -1] = np.sum(freq, axis=1)
-
-        # Fréquece de visite d'un état à partir d'un autre état
-        freq = pd.DataFrame(freq[1:, 1:], columns=header2, index=header1)
-        freq.columns.name = 'Freq'
-
-        print(freq)
-
-    def montecarlo_SMC(self, eps=0.01, delta=0.05, max_depth = 5):
-        N = int((np.log(2) - np.log(delta))/(2*eps)**2)
-        freq = np.zeros(len(self.states))
-        for state in self.states:
-            for _ in range(N):
-                s = self._parcoursRapideAlea(N_pas=max_depth)
-                if s == self.dictStates[state]:
-                    freq[s] += 1
-        return freq/N 
-    
-    def sprt_SMC(self, propriété,alpha=0.01, beta=0.01, theta=0.5, eps=0.01, max_depth=5):
-        gamma1 = theta - eps
-        gamma0 = theta + eps
-        A = np.log(1-beta) - np.log(alpha)
-        B = np.log(beta) - np.log(1-alpha)
-        Rm = 0
-        Rtrue = np.log(gamma1) - np.log(gamma0)
-        Rfalse = np.log(1-gamma1) - np.log(1-gamma0)
-        i = 0
-        while Rm < A and Rm > B :
-            i += 1
-            s = self._parcoursRapideAlea(N_pas = max_depth)
-            if propriété(self.states[s]) :
-                Rm += Rtrue
-            else :
-                Rm += Rfalse 
-
-        print(f"i = {i} itérations")
-        print(f"A : {A}, B : {B} et Rm : {Rm}")
-        if Rm >= A :
-            return f"H1 : gamma < {theta} accepté"
-        elif Rm <= B :
-            return f"H0 : gamma >= {theta} accepté"
-        
-    def iter_valeurs(self, gamma, eps = 0.1):
-        pass
-
-    def iter_politique(self, gamma):
-        pass
-    
